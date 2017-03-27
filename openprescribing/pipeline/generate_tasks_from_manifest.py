d3b27c60d7a1ec5bf641217b075e72698533b6e2
Peter Inglesby <peter.inglesby@gmail.com>
2017-03-27 17:00:32 +0100

    Add generate_tasks_from_manifest.py and its output

---
+++ openprescribing/pipeline/generate_tasks_from_manifest.py
@@ -0,0 +1,161 @@
+from __future__ import print_function
+
+import json
+
+import networkx as nx
+
+def to_camel_case(s):
+    return s.replace('_', ' ').title().replace(' ', '')
+
+with open('manifest.json') as f:
+    sources = json.load(f)
+
+print('There are {} sources'.format(len(sources)))
+
+tasks = []
+
+for source in sources:
+    source_id = source['id']
+
+    manual_fetcher = False
+    fetcher = source.get('fetcher')
+    if fetcher:
+        tasks.append({
+            'id': 'fetch_{}'.format(source_id),
+            'source': source_id,
+            'type': 'fetcher',
+            'command': fetcher,
+            'dependencies': [],
+        })
+    elif 'core_data' in source['tags'] and source.get('publisher'):
+        manual_fetcher = True
+        tasks.append({
+            'id': 'fetch_{}'.format(source_id),
+            'source': source_id,
+            'type': 'manual_fetcher',
+            'command': None,
+            'dependencies': [],
+        })
+
+    before_tasks = []
+    before_import = source.get('before_import', [])
+    if len(before_import) == 1:
+        assert source['publisher'] == ''
+        before_tasks.append({
+            'id': source_id,
+            'source': None,
+            'type': 'other',
+            'command': before_import[0],
+            'dependencies': source.get('depends_on', []),
+        })
+    else:
+        for ix, command in enumerate(before_import):
+            assert source['publisher'] == ''
+            before_tasks.append({
+                'id': '{}_{}'.format(source_id, ix),
+                'source': None,
+                'type': 'other',
+                'command': command,
+                'dependencies': source.get('depends_on', []),
+            })
+
+    tasks.extend(before_tasks)
+
+    importer_dependencies = []
+    if fetcher or manual_fetcher:
+        importer_dependencies.append('fetch_{}'.format(source_id))
+
+    if before_tasks:
+        importer_dependencies.extend(before_tasks)
+    else:
+        importer_dependencies.extend(source.get('depends_on', []))
+
+    importers = source.get('importers', [])
+    if len(importers) == 1:
+        importer_id = 'import_{}'.format(source_id)
+        tasks.append({
+            'id': importer_id,
+            'source': source_id,
+            'type': 'importer',
+            'command': importers[0],
+            'dependencies': importer_dependencies,
+        })
+        importer_ids = [importer_id]
+    else:
+        importer_ids = []
+        for ix, importer in enumerate(importers):
+            importer_id = 'import_{}_{}'.format(source_id, ix)
+            tasks.append({
+                'id': importer_id,
+                'source': source_id,
+                'type': 'importer',
+                'command': importer,
+                'dependencies': importer_dependencies,
+            })
+            importer_ids.append(importer_id)
+
+    if importers:
+        tasks.append({
+            'id': source_id,
+            'source': source_id,
+            'type': 'dummy',
+            'command': None,
+            'dependencies': importer_ids,
+        })
+
+task_ids = [task['id'] for task in tasks]
+
+for task in tasks:
+    if task['type'] == 'dummy':
+        for task1 in tasks:
+            if task['id'] in task1['dependencies']:
+                task1['dependencies'].remove(task['id'])
+                task1['dependencies'].extend(task['dependencies'])
+
+tasks = [task for task in tasks if task['type'] != 'dummy']
+
+for task in tasks:
+    for dependency_id in task['dependencies']:
+        assert dependency_id in task_ids, 'Unknown dependency: {}'.format(dependency_id)
+
+graph = nx.DiGraph()
+for task in tasks:
+    graph.add_node(task['id'])
+    for dependency in task['dependencies']:
+        graph.add_node(dependency)
+        graph.add_edge(dependency, task['id'])
+
+ordered_task_ids = nx.topological_sort(graph)
+tasks = sorted(tasks, key=lambda t: ordered_task_ids.index(t['id']))
+
+# with open('tasks.json', 'w') as f:
+#     json.dump(tasks, f, indent=4)
+
+with open('task_definitions.py', 'w') as f:
+    f.write('class TaskDefinition(object): pass')
+
+    for task in tasks:
+        f.write('\n\n\n')
+        lines = [
+            'class {}(TaskDefinition):'.format(to_camel_case(task['id'])),
+            "    task_type = '{}'".format(task['type']),
+        ]
+
+        if task['source']:
+            lines.append("    source = '{}'".format(task['source']))
+
+        if task['dependencies']:
+            lines.append('    dependencies = [')
+            for dependency in task['dependencies']:
+                lines.append('        {},'.format(to_camel_case(dependency)))
+            lines.append('    ]')
+
+        if task['command']:
+            lines.append('')
+            lines.append('    def run(self):')
+            lines.append("        '''{}'''".format(task['command']))
+
+        code = '\n'.join(lines)
+        f.write(code)
+
+print('There are {} tasks'.format(len(tasks)))
---
+++ openprescribing/pipeline/task_definitions.py
@@ -0,0 +1,282 @@
+class TaskDefinition(object): pass
+
+
+class FetchBnfCodes(TaskDefinition):
+    task_type = 'manual_fetcher'
+    source = 'bnf_codes'
+
+
+class FetchAdqs(TaskDefinition):
+    task_type = 'manual_fetcher'
+    source = 'adqs'
+
+
+class FetchPatientListSize(TaskDefinition):
+    task_type = 'manual_fetcher'
+    source = 'patient_list_size'
+
+
+class FetchCcgDetails(TaskDefinition):
+    task_type = 'fetcher'
+    source = 'ccg_details'
+
+    def run(self):
+        '''org_codes.py --ccg'''
+
+
+class FetchPracticeDetails(TaskDefinition):
+    task_type = 'fetcher'
+    source = 'practice_details'
+
+    def run(self):
+        '''org_codes.py --practice'''
+
+
+class ImportBnfCodes(TaskDefinition):
+    task_type = 'importer'
+    source = 'bnf_codes'
+    dependencies = [
+        FetchBnfCodes,
+    ]
+
+    def run(self):
+        '''import_bnf_codes --filename bnf_codes.csv'''
+
+
+class FetchCcgBoundaries(TaskDefinition):
+    task_type = 'manual_fetcher'
+    source = 'ccg_boundaries'
+
+
+class ImportCcgBoundaries(TaskDefinition):
+    task_type = 'importer'
+    source = 'ccg_boundaries'
+    dependencies = [
+        FetchCcgBoundaries,
+    ]
+
+    def run(self):
+        '''import_ccg_boundaries --filename ccg_boundaries.*\.kml'''
+
+
+class ImportCcgDetails(TaskDefinition):
+    task_type = 'importer'
+    source = 'ccg_details'
+    dependencies = [
+        FetchCcgDetails,
+        ImportCcgBoundaries,
+    ]
+
+    def run(self):
+        '''import_org_names --ccg eccg.csv'''
+
+
+class FetchPrescribing(TaskDefinition):
+    task_type = 'fetcher'
+    source = 'prescribing'
+
+    def run(self):
+        '''hscic_prescribing.py --most_recent_date'''
+
+
+class ImportAdqs(TaskDefinition):
+    task_type = 'importer'
+    source = 'adqs'
+    dependencies = [
+        FetchAdqs,
+        ImportBnfCodes,
+    ]
+
+    def run(self):
+        '''import_adqs --filename adqs_.*csv'''
+
+
+class FetchPatientListWeightings(TaskDefinition):
+    task_type = 'manual_fetcher'
+    source = 'patient_list_weightings'
+
+
+class ImportPatientListWeightings(TaskDefinition):
+    task_type = 'importer'
+    source = 'patient_list_weightings'
+    dependencies = [
+        FetchPatientListWeightings,
+    ]
+
+    def run(self):
+        '''calculate_star_pu_weights --filename prescribing_units.xlsx'''
+
+
+class ImportPracticeDetails(TaskDefinition):
+    task_type = 'importer'
+    source = 'practice_details'
+    dependencies = [
+        FetchPracticeDetails,
+        ImportCcgDetails,
+        ImportPatientListWeightings,
+    ]
+
+    def run(self):
+        '''import_practices --epraccur epraccur.csv'''
+
+
+class FetchNhsPostcodeFile(TaskDefinition):
+    task_type = 'fetcher'
+    source = 'nhs_postcode_file'
+
+    def run(self):
+        '''org_codes.py --postcode'''
+
+
+class ImportNhsPostcodeFile(TaskDefinition):
+    task_type = 'importer'
+    source = 'nhs_postcode_file'
+    dependencies = [
+        FetchNhsPostcodeFile,
+        ImportPracticeDetails,
+    ]
+
+    def run(self):
+        '''geocode_practices --filename gridall\.csv'''
+
+
+class ImportPrescribing3(TaskDefinition):
+    task_type = 'importer'
+    source = 'prescribing'
+    dependencies = [
+        FetchPrescribing,
+        ImportBnfCodes,
+        ImportAdqs,
+        ImportNhsPostcodeFile,
+        ImportCcgDetails,
+        ImportPracticeDetails,
+    ]
+
+    def run(self):
+        '''import_hscic_prescribing --filename T\d+PDPI.*_formatted\.CSV'''
+
+
+class ImportPrescribing2(TaskDefinition):
+    task_type = 'importer'
+    source = 'prescribing'
+    dependencies = [
+        FetchPrescribing,
+        ImportBnfCodes,
+        ImportAdqs,
+        ImportNhsPostcodeFile,
+        ImportCcgDetails,
+        ImportPracticeDetails,
+    ]
+
+    def run(self):
+        '''convert_hscic_prescribing --filename T\d+PDPI.*BNFT\.CSV'''
+
+
+class ImportPrescribing1(TaskDefinition):
+    task_type = 'importer'
+    source = 'prescribing'
+    dependencies = [
+        FetchPrescribing,
+        ImportBnfCodes,
+        ImportAdqs,
+        ImportNhsPostcodeFile,
+        ImportCcgDetails,
+        ImportPracticeDetails,
+    ]
+
+    def run(self):
+        '''import_practices --hscic_address T\d+ADDR.*\.CSV'''
+
+
+class ImportPrescribing0(TaskDefinition):
+    task_type = 'importer'
+    source = 'prescribing'
+    dependencies = [
+        FetchPrescribing,
+        ImportBnfCodes,
+        ImportAdqs,
+        ImportNhsPostcodeFile,
+        ImportCcgDetails,
+        ImportPracticeDetails,
+    ]
+
+    def run(self):
+        '''import_hscic_chemicals --chem_file T\d+CHEM.*\.CSV'''
+
+
+class ImportDispensingPractices(TaskDefinition):
+    task_type = 'importer'
+    source = 'dispensing_practices'
+    dependencies = [
+        ImportPrescribing0,
+        ImportPrescribing1,
+        ImportPrescribing2,
+        ImportPrescribing3,
+        ImportPracticeDetails,
+    ]
+
+    def run(self):
+        '''import_practice_dispensing_status --filename dispensing_practices.*\.csv'''
+
+
+class ImportPatientListSize(TaskDefinition):
+    task_type = 'importer'
+    source = 'patient_list_size'
+    dependencies = [
+        FetchPatientListSize,
+        ImportPrescribing0,
+        ImportPrescribing1,
+        ImportPrescribing2,
+        ImportPrescribing3,
+        ImportPracticeDetails,
+        ImportPatientListWeightings,
+    ]
+
+    def run(self):
+        '''import_list_sizes --filename Patient_List_Size.*csv'''
+
+
+class UploadToBigquery(TaskDefinition):
+    task_type = 'other'
+    dependencies = [
+        ImportPatientListSize,
+        ImportPrescribing0,
+        ImportPrescribing1,
+        ImportPrescribing2,
+        ImportPrescribing3,
+        ImportCcgDetails,
+        ImportPracticeDetails,
+    ]
+
+    def run(self):
+        '''runner:bigquery_upload'''
+
+
+class Measures0(TaskDefinition):
+    task_type = 'other'
+    dependencies = [
+        UploadToBigquery,
+    ]
+
+    def run(self):
+        '''import_measures --definitions_only'''
+
+
+class Measures1(TaskDefinition):
+    task_type = 'other'
+    dependencies = [
+        UploadToBigquery,
+    ]
+
+    def run(self):
+        '''import_measures'''
+
+
+class RefreshViews(TaskDefinition):
+    task_type = 'other'
+    dependencies = [
+        UploadToBigquery,
+    ]
+
+    def run(self):
+        '''create_views'''
